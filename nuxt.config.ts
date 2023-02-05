cd1517fd3ad82c1c2ab28b0ff8a1f7c8fd5e44dc
rp-guruh <github.gurh@gmail.com>
2023-02-05 20:05:57 +0700

    edit config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  nitro: {
+    preset: 'vercel-edge',
+  },
   css: ["~/assets/css/main.css", "vuetify/lib/styles/main.sass"],
   build: {
     transpile: ["vuetify"],
@@ -15,6 +18,4 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
-  
-
 });

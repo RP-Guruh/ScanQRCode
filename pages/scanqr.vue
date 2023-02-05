f63e1457d253e316a9a901fedd05f4fcacc71a59
rp-guruh <github.gurh@gmail.com>
2023-02-05 11:43:30 +0700

    delete plugin

--- pages/scanqr.vue
+++ pages/scanqr.vue
@@ -1,29 +1,3 @@
-<script setup>
-import { ref } from "vue";
-import { StreamBarcodeReader } from "vue-barcode-reader";
-
-const decodedText = ref("");
-
-const onLoaded = () => {
-  console.log("loaded");
-};
-
-const onDecode = (text) => {
-  decodedText.value = text;
-};
-</script>
-
 <template>
-  <StreamBarcodeReader @decode="onDecode" @loaded="onLoaded"></StreamBarcodeReader>
-  <h2>The result value in QR/barcode is</h2>
-  <h2>{{ decodedText }}</h2>
+  <h2>QR PAGE</h2>
 </template>
-
-<style scoped>
-a {
-  color: #42b983;
-}
-.information {
-  margin-top: 100px;
-}
-</style>

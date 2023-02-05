d1069e58bdfb9b3d852f21c053406fde67756b8e
pink_Key <46945652+RP-Guruh@users.noreply.github.com>
2023-02-05 23:50:16 +0700

    update config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-//  nitro: {
-  //  preset: 'vercel-edge',
-  //},
+  nitro: {
+    preset: 'vercel-edge',
+  },
   modules: ['@nuxtjs/color-mode'],
   css: ["~/assets/css/main.css", "vuetify/lib/styles/main.sass"],
   build: {
